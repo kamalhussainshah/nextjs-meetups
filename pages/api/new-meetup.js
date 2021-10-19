702fdea4438728549c672b07a011ded399034bde
kamalhussainshah <kamalshah205@gmail.com>
2021-10-19 15:35:03 +0500

    next

---
+++ pages/api/new-meetup.js
@@ -0,0 +1,29 @@
+import { MongoClient } from "mongodb";
+
+// /api/new-meetup
+// POST /api/new-meetup
+
+async function handler(req, res) {
+  if (req.method === "POST") {
+    const data = req.body;
+
+    const client = await MongoClient.connect(
+      "mongodb+srv://kamal:Reactjs786@cluster0.6x0vx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+      { useUnifiedTopology: true }
+    );
+
+    const db = client.db();
+
+    const meetupsCollection = db.collection("meetups");
+
+    const result = await meetupsCollection.insertOne(data);
+
+    console.log(result);
+
+    client.close();
+
+    res.status(201).json({ message: "Meetup inserted!" });
+  }
+}
+
+export default handler;
